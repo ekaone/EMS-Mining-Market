c82f6fa96505d86383e7567dda88bcf5b9c02a63
ekaone <ekaone3033@gmail.com>
2019-12-21 14:44:58 +0700

    👀

---
+++ src/pages/Landing/Landing.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import logoems from '../../assets/Landing/logoems.svg';
+import '../../styles/Landing/Landing.css' 
+
+export default function Landing() {
+  return (
+    <>
+      <div className="App">
+      <header className="App-header">
+        <img src={logoems} className="App-logo" alt="logo" />
+        <p>
+          <b>EMS</b> Mining Market solution on your Business
+        </p>
+        <a
+          className="App-link"
+          href="https://adiraja-integrasi.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn more
+        </a>
+      </header>
+    </div>
+    </>
+  )
+}
