f82569634f07161b939acca6d458c7741f4d5cd3
ekaone <ekaone3033@gmail.com>
2019-12-23 18:14:28 +0700

    👀

--- src/views/Landing/variables.js
+++ src/views/Landing/variables.js
@@ -1,4 +1,14 @@
 export const cards = [
-  { id: 1, heading: 'About Us', imgsrc: 'https://source.unsplash.com/random/800x600', subheading: 'Adiraja Integrasi is a subsidiary of EMS Paramitra consulting company, a leading service provider in Indonesia with asset intensive industry expertise.', path: 'https://adiraja-integrasi.com/about-us/' },
-  { id: 2, heading: 'Solutions', imgsrc: 'https://source.unsplash.com/random/600x400', subheading: 'Enterprise Resource Planning, Mining Supply Chain Solution, Real Estate Management Solution, Human Resource Information System and more.', path: 'https://adiraja-integrasi.com/products/' }
+  { 
+    id: 1, 
+    heading: 'About Us', 
+    imgsrc: 'https://source.unsplash.com/random/800x600', 
+    subheading: 'Adiraja Integrasi is a subsidiary of EMS Paramitra consulting company, a leading service provider in Indonesia with asset intensive industry expertise.', 
+    path: 'https://adiraja-integrasi.com/about-us/' },
+  { 
+    id: 2, 
+    heading: 'Solutions', 
+    imgsrc: 'https://source.unsplash.com/random/600x400', 
+    subheading: 'Enterprise Resource Planning, Mining Supply Chain Solution, Real Estate Management Solution, Human Resource Information System and more.', 
+    path: 'https://adiraja-integrasi.com/products/' }
 ];
